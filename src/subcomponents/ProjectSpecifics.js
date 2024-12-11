d50f2080c8c319b93aad83d11758c90de118879d
Declan McKoen <dmckoen@ufl.edu>
2024-12-11 13:07:04 -0500

    changes

--- src/subcomponents/ProjectSpecifics.js
+++ src/subcomponents/ProjectSpecifics.js
@@ -5,7 +5,6 @@ import useFetch from "../hooks/useFetch";
 const ProjectSpecifics = () => {
   const { id } = useParams();
   const { data, error } = useFetch("/portfolio/data/db.json");
-  console.log(data);
 
   const project = data?.projects?.find(project => project.id === id);
 
@@ -13,7 +12,35 @@ const ProjectSpecifics = () => {
     return <p>Loading...</p>;
   }
   return (
-    <p>{ project && project.about }</p>
+    <div className="project-specifics-wrapper">
+      <h1>{ project.name }</h1>
+      <div className="link">
+        <a href={ project.github } target="_blank">Link to repository</a>
+      </div>
+      <div className="project-specifics">
+        <div className="project-about">
+          <h3>About</h3>
+          { project.about }
+        </div>
+        <div className="project-technical">
+          <h3>Technical Details</h3>
+          { project.technical }
+        </div>
+        <div className="skills-used">
+          <h3>Skills Used</h3>
+          <ul>
+            { project.skills.map((skill, index) => {
+              return <li key={index}>{ skill }</li>
+            })}
+          </ul>
+        </div>
+        <div className="pictures">
+          { project.pictures.map((picture, index) => {
+            return <img src={picture} alt={picture} key={index} />
+          })}
+        </div>
+      </div>
+    </div>
   );
 }
  
